42c3c8a1914490fcd368a8d97a62deda42554c02
piyush-0p <145971445+piyush-0p@users.noreply.github.com>
2025-08-14 17:47:53 +0530

    Initial Commit

---
+++ CryptoUI/CryptoUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoUIApp.swift
+//  CryptoUI
+//
+//  Created by Piyush on 14/08/25.
+//
+
+import SwiftUI
+
+@main
+struct CryptoUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
